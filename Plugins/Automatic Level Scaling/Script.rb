7b5135b6a631338bc58c7ff925b4d46a698ebab8
Benitex <benitoapepe@yahoo.com.br>
2022-05-23 17:59:16 -0300

    Atualizado Automatic Level Scaling

--- Plugins/Automatic Level Scaling/Script.rb
+++ Plugins/Automatic Level Scaling/Script.rb
@@ -5,117 +5,119 @@
 
 Events.onWildPokemonCreate += proc { |_sender, e|
   pokemon = e[0]
-  difficulty = $game_variables[LevelScalingSettings::WILD_VARIABLE] 
-
-  # Make all wild Pokémon shiny while a certain Switch is ON (see Pokemon Essentials Settings script).
-  pokemon.shiny = true if $game_switches[Settings::SHINY_WILD_POKEMON_SWITCH]
-
-  setNewLevel(pokemon, difficulty) if difficulty != 0
+  selectedDifficultyID = $game_variables[LevelScalingSettings::WILD_VARIABLE]
+  if selectedDifficultyID != 0
+    AutomaticLevelScaling.setDifficulty(selectedDifficultyID)
+    AutomaticLevelScaling.setNewLevel(pokemon)
+  end
 }
 
 Events.onTrainerPartyLoad += proc { |_sender, trainer|
   if trainer   # An NPCTrainer object containing party/items/lose text, etc.
-    difficulty = $game_variables[LevelScalingSettings::TRAINER_VARIABLE]
-    if difficulty != 0
+    selectedDifficultyID = $game_variables[LevelScalingSettings::TRAINER_VARIABLE]
+    if selectedDifficultyID != 0
+      AutomaticLevelScaling.setDifficulty(selectedDifficultyID)
       avarage_level = 0
       trainer[0].party.each { |pokemon| avarage_level += pokemon.level }
       avarage_level /= trainer[0].party.length
 
       for pokemon in trainer[0].party
-        setNewLevel(pokemon, difficulty, pokemon.level - avarage_level)
+        AutomaticLevelScaling.setNewLevel(pokemon, pokemon.level - avarage_level)
       end
     end
   end
 }
 
-def setNewLevel(pokemon, selectedDifficulty, difference_from_average = 0)
-  new_level = pbBalancedLevel($Trainer.party) - 2 # pbBalancedLevel increses level by 2 to challenge the player
+class AutomaticLevelScaling
+  @@selectedDifficulty
+  @@settings = {
+    automatic_evolutions: true,
+    first_evolution_level: 20,
+    second_evolution_level: 40,
+    update_moves: true,
+    proportional_scaling: false
+  }
 
-  # Difficulty modifiers
-  for difficulty in LevelScalingSettings::DIFICULTIES do
-    if difficulty.id == selectedDifficulty
-      new_level += difficulty.fixed_increase
-      if difficulty.random_increase < 0
-        new_level += rand(difficulty.random_increase..0)
-      elsif difficulty.random_increase > 0
-        new_level += rand(difficulty.random_increase)
+  def self.setDifficulty(selectedDifficultyID)
+    for difficulty in LevelScalingSettings::DIFICULTIES do
+      @@selectedDifficulty = difficulty if difficulty.id == selectedDifficultyID
+    end
+  end
+
+  def self.setNewLevel(pokemon, difference_from_average = 0)
+    new_level = pbBalancedLevel($Trainer.party) - 2 # pbBalancedLevel increses level by 2 to challenge the player
+
+    # Difficulty modifiers
+    new_level += @@selectedDifficulty.fixed_increase
+    if @@selectedDifficulty.random_increase < 0
+      new_level += rand(@@selectedDifficulty.random_increase..0)
+    elsif @@selectedDifficulty.random_increase > 0
+      new_level += rand(@@selectedDifficulty.random_increase)
+    end
+    # Proportional scaling
+    new_level += difference_from_average if LevelScalingSettings::PROPORTIONAL_SCALING || @@settings[:proportional_scaling]
+
+    new_level = new_level.clamp(1, GameData::GrowthRate.max_level)
+    pokemon.level = new_level
+
+    # Evolution part
+    AutomaticLevelScaling.setNewStage(pokemon) if LevelScalingSettings::AUTOMATIC_EVOLUTIONS || @@settings[:automatic_evolutions]
+
+    pokemon.calc_stats
+    pokemon.reset_moves if @@settings[:update_moves]
+  end
+
+  def self.setNewStage(pokemon)
+    pokemon.species = GameData::Species.get(pokemon.species).get_baby_species # revert to the first stage
+
+    2.times do |evolvedTimes|
+      evolutions = GameData::Species.get(pokemon.species).get_evolutions(false)
+
+      # Checks if the species only evolve by level up
+      other_evolving_method = false
+      evolutions.length.times { |i|
+        if evolutions[i][1] != :Level
+          other_evolving_method = true
+        end
+      }
+
+      if !other_evolving_method   # Species that evolve by level up
+        if pokemon.check_evolution_on_level_up != nil
+          pokemon.species = pokemon.check_evolution_on_level_up
+        end
+
+      else  # For species with other evolving methods
+        # Checks if the pokemon is in it's midform and defines the level to evolve
+        level = evolvedTimes == 0 ? @@settings[:second_evolution_level] : @@settings[:second_evolution_level]
+
+        if pokemon.level >= level
+          if evolutions.length == 1     # Species with only one possible evolution
+            pokemon.species = evolutions[0][0]
+          elsif evolutions.length > 1   # Species with multiple possible evolutions (the evolution is randomly defined)
+            pokemon.species = evolutions[rand(0, evolutions.length - 1)][0]
+          end
+        end
       end
     end
   end
 
-  new_level += difference_from_average if LevelScalingSettings::PROPORTIONAL_SCALING
-  new_level = new_level.clamp(1, GameData::GrowthRate.max_level)
-  pokemon.level = new_level
-
-  if LevelScalingSettings::AUTOMATIC_EVOLUTIONS
-    setNewStage(pokemon, selectedDifficulty)  # Evolution part
-  end
-  pokemon.calc_stats
-  pokemon.reset_moves if LevelScalingSettings::UPDATE_MOVES
-end
-
-def setNewStage(pokemon, selectedDifficulty)
-  pokemon.species = GameData::Species.get(pokemon.species).get_baby_species # revert to the first stage
-
-  2.times do |evolvedTimes|
-    evolutions = GameData::Species.get(pokemon.species).get_evolutions(false)
-
-    # Checks if the species only evolve by level up
-    other_evolving_method = false
-    i = 0
-    while i < evolutions.length
-      if evolutions[i][1] != :Level
-        other_evolving_method = true
-      end
-      i += 1
-    end
-
-    # Species that evolve by level up
-    if !other_evolving_method
-      if pokemon.check_evolution_on_level_up != nil
-        pokemon.species = pokemon.check_evolution_on_level_up
-      end
-
-    # For species with other evolving methods
-    else
-      # Checks if the pokemon is in it's midform and defines the level to evolve
-      if evolvedTimes == 0
-        for difficulty in LevelScalingSettings::DIFICULTIES do
-          if difficulty.id == selectedDifficulty
-            level = difficulty.first_evolution_level
-          end
-        end
-      else
-        for difficulty in LevelScalingSettings::DIFICULTIES do
-          if difficulty.id == selectedDifficulty
-            level = difficulty.secund_evolution_level
-          end
-        end
-      end
-
-      # Species with only one possible evolution
-      if evolutions.length == 1 && pokemon.level >= level
-        pokemon.species = evolutions[0][0]
-      # Species with only multiple possible evolutions (the evolution is defined randomly)
-      elsif evolutions.length > 1 && pokemon.level >= level
-        pokemon.species = evolutions[rand(0, evolutions.length - 1)][0]
-      end
-    end
+  def self.setSettings(update_moves: true, automatic_evolutions: LevelScalingSettings::AUTOMATIC_EVOLUTIONS, proportional_scaling: LevelScalingSettings::PROPORTIONAL_SCALING, first_evolution_level: LevelScalingSettings::DEFAULT_FIRST_EVOLUTION_LEVEL, second_evolution_level: LevelScalingSettings::DEFAULT_SECOND_EVOLUTION_LEVEL)
+    @@settings[:update_moves] = update_moves
+    @@settings[:first_evolution_level] = first_evolution_level
+    @@settings[:second_evolution_level] = second_evolution_level
+    @@settings[:proportional_scaling] = proportional_scaling
+    @@settings[:automatic_evolutions] = automatic_evolutions
   end
 end
 
 class Difficulty
   attr_accessor :id
-  attr_accessor :random_increase
   attr_accessor :fixed_increase
-  attr_accessor :first_evolution_level
-  attr_accessor :secund_evolution_level
+  attr_accessor :random_increase
 
-  def initialize(id, random_increase, fixed_increase, first_evolution_level = LevelScalingSettings::DEFAULT_FIRST_EVOLUTION_LEVEL, secund_evolution_level = LevelScalingSettings::DEFAULT_SECOND_EVOLUTION_LEVEL)
+  def initialize(id:, fixed_increase: 0, random_increase: 0)
     @id = id
     @random_increase = random_increase
     @fixed_increase = fixed_increase
-    @first_evolution_level = first_evolution_level
-    @secund_evolution_level = secund_evolution_level
   end
 end
--- Plugins/Automatic Level Scaling/Settings.rb
+++ Plugins/Automatic Level Scaling/Settings.rb
@@ -9,28 +9,24 @@ module LevelScalingSettings
   TRAINER_VARIABLE = 99
   WILD_VARIABLE = 100
 
+  # If evolution levels are not defined when creating a difficulty, these are the default values used
   AUTOMATIC_EVOLUTIONS = true
-  UPDATE_MOVES = true
+  DEFAULT_FIRST_EVOLUTION_LEVEL = 20
+  DEFAULT_SECOND_EVOLUTION_LEVEL = 40
+
   # Scales levels but takes original level differences into consideration
   # Don't forget to set random_increase values to 0 when using this setting
   PROPORTIONAL_SCALING = false
 
-  # If evolution levels are not defined when creating a difficulty, these are the default values used
-  DEFAULT_FIRST_EVOLUTION_LEVEL = 20
-  DEFAULT_SECOND_EVOLUTION_LEVEL = 40
-
-  # You can add your own difficulties here, using the function "Difficulty.new(id, random_increase, fixed_increase, (optional) first_evolution_level, (optional) second_evolution_level)"
+  # You can add your own difficulties here, using the function "Difficulty.new(id, fixed_increase, random_increase, first_evolution_level, second_evolution_level)"
   #   "id" is the value stored in TRAINER_VARIABLE or WILD_VARIABLE, defines the active difficulty
-  #   "random_increase" is a random value that increases the level
-  #   "fixed_increase" is a pre defined value that increases the level
-  #   "first_evolution_level" is the level required for pokemon that don't evolve by level to get to the mid form
-  #   "second_evolution_level" is the level required for pokemon that don't evolve by level to get to the final form
+  #   "fixed_increase" is a pre defined value that increases the level (optional)
+  #   "random_increase" is a random value that increases the level (optional)
   # Note that these variables can also store negative values
   DIFICULTIES = [
-    Difficulty.new(1, 2, -2),         # Easy
-    Difficulty.new(2, 2, 0),          # Medium
-    Difficulty.new(3, 3, 3, 15, 30),  # Hard
-    Difficulty.new(4, 0, 0)           # Avarage
+    Difficulty.new(id: 1, fixed_increase: -2, random_increase: 2),  # Easy
+    Difficulty.new(id: 2, random_increase: 2),                      # Medium
+    Difficulty.new(id: 3, fixed_increase: 3, random_increase: 3),   # Hard
   ]
 
 end
