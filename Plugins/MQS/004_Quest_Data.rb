888710108827fb016ca088b08b13b5c485daf446
Benitex <benitoapepe@yahoo.com.br>
2022-06-11 11:12:55 -0300

    Conversa com a Jasmine sobre os Guardiões

--- Plugins/MQS/004_Quest_Data.rb
+++ Plugins/MQS/004_Quest_Data.rb
@@ -2,8 +2,7 @@ module QuestModule
   
   # You don't actually need to add any information, but the respective fields in the UI will be blank or "???"
   # I included this here mostly as an example of what not to do, but also to show it's a thing that exists
-  Quest0 = {
-  }
+  Quest0 = { }
 
   Quest1 = {
     :ID => "1",
