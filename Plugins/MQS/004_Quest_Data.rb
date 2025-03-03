acaeb1f99994221387d529f08305513c14dc62f3
Benitex <benitoapepe@yahoo.com.br>
2025-03-03 19:54:10 -0300

    Adicionada tradução das quests

--- Plugins/MQS/004_Quest_Data.rb
+++ Plugins/MQS/004_Quest_Data.rb
@@ -93,7 +93,7 @@ module QuestModule
     :Stage1 => "Encontre o professor Carvalho.",
     :Stage2 => "Enfrente os motoqueiros.",
     :Stage3 => "Converse com o professor Carvalho.",
-    :Stage4 => "Procure o Relic Castle.",
+    :Stage4 => "Investigue o Relic Castle.",
     :Stage5 => "Converse com o professor Carvalho.",
     :Stage6 => "Explore os esgotos.",
     :Stage7 => "Explore o Relic Castle.",
@@ -192,7 +192,7 @@ module QuestModule
 	  :Stage2 => "Devolva para o pai.",
     :Location1 => "Rota 4",
 	  :Location2 => "Rota 4",
-    :QuestDescription => "Uma criança perdeu um onix de brinquedo no meio das pedras da Rota 4 e preciso devolver para o pai, imagino o que mais pode haver nessas pedras.",
+    :QuestDescription => "Uma criança perdeu um onix de brinquedo no meio das pedras da Rota 4 e preciso devolver para o pai. Imagino o que mais pode haver nessas pedras.",
   }
 
   Quest16 = {
@@ -203,13 +203,13 @@ module QuestModule
 	  :Stage2 => "Diga o que aconteceu.",
     :Location1 => "Rota 4",
 	  :Location2 => "Rota 4",
-    :QuestDescription => "Um ciêntista pervertido me pediu para entregar uma carta de amor para uma menina na praia.",
+    :QuestDescription => "Um ciêntista pervertido me pediu para entregar uma carta de amor para uma menina na praia da Rota 4.",
   }
 
   Quest17 = {
     :ID => "17",
     :Name => "Acabando com os motoqueiros!",
-    :QuestGiver => "Polícia",
+    :QuestGiver => "Delegacia de Castelia",
     :Stage1 => "Pegue informações no quartel.",
     :Stage2 => "Invada a cidade dos motoqueiros.",
     :Stage3 => "Enfrente o chefe dos motoqueiros.",
@@ -220,7 +220,7 @@ module QuestModule
     :Location3 => "Cidade dos motoqueiros",
     :Location4 => "Cidade dos motoqueiros",
     :Location5 => "Cidade dos motoqueiros",
-    :QuestDescription => "Os motoqueiros da Rota 4 estão causando confusão para todos, o melhor a se fazer é acabar com essa bagunça. Devo decidir o que fazer com os guardas em Castelia e prosseguir com o ataque em defesa da Rota 4.",
+    :QuestDescription => "Motoqueiros estão causando confusão na Rota 4, o melhor a se fazer é acabar com essa bagunça. Devo decidir o que fazer com os guardas em Castelia e prosseguir com o ataque em defesa da Rota 4.",
   }
 
   Quest18 = {
@@ -229,7 +229,7 @@ module QuestModule
     :QuestGiver => "Velho Sábio",
     :Stage1 => "Capture 10 pokemon diferentes.",
     :Location1 => "Laboratório da Juniper",
-    :QuestDescription => "Um senhor que já passou por muitas histórias me desafiou a mostrar 10 espécies diferentes na pokédex e em troca, me dará um pokemon que vem de fora do sul de Unova.",
+    :QuestDescription => "Um senhor que me desafiou a mostrar 10 espécies diferentes na pokédex e em troca, me dará um pokemon que vem de fora do sul de Unova.",
   }
   
   Quest19 = {
@@ -259,20 +259,20 @@ module QuestModule
   Quest21 = {
     :ID => "21",
     :Name => "Pokemon desaparecidos",
-    :Stage1 => "Encontre os pokemon",
-    :Stage2 => "Liberte os pokemon",
+    :Stage1 => "Encontre os pokemon.",
+    :Stage2 => "Liberte os pokemon.",
     :Location1 => "???",
     :Location2 => "Cidade dos Motoqueiros",
-    :QuestDescription => "Vários treinadores de Castelia estão reportando que seus pokemon estão desaparecendo das boxes. A maioria acredita que sejam os motoqueiros, que estão causando muitas confusões ultimamente. Preciso resolver esse problema e trazer esses pokemon de volta.",
+    :QuestDescription => "Vários treinadores de Castelia estão reportando que seus pokemon estão desaparecendo das boxes. Preciso resolver esse problema e trazer esses pokemon de volta.",
   }
 
   Quest22 = {
     :ID => "22",
     :Name => "Reerguendo a tenda de berrys!",
     :QuestGiver => "Vendedora de berrys",
-    :Stage1 => "Doe 500$",
-    :Stage2 => "Doe 1000$",
-    :Stage3 => "Doe 2000$",
+    :Stage1 => "Doe 500$.",
+    :Stage2 => "Doe 1000$.",
+    :Stage3 => "Doe 2000$.",
     :Location1 => "Café Sonata",
     :Location2 => "Oceanfront Road",
     :Location3 => "Oceanfront Road",
@@ -283,7 +283,7 @@ module QuestModule
     :ID => "23",
     :Name => "Pérola pro velhinho",
     :QuestGiver => "Velhinho",
-    :Stage1 => "Entregue uma pérola",
+    :Stage1 => "Entregue uma pérola.",
     :Location1 => "Central Plaza",
     :QuestDescription => "Um velhinho na Central Plaza me pediu para pegar uma pérola e entregar para ele, ele acredita que a esposa vai ficar mais feliz ao se lembrar do primeiro presente que recebeu dele.",
   }
@@ -291,9 +291,9 @@ module QuestModule
   Quest24 = {
     :ID => "24",
     :Name => "Filho de peixe, peixinho é",
-    :QuestGiver => "Pescador do Liberty Pier",
-    :Stage1 => "Encontre o tio do pescador",
-    :Stage2 => "Leve um Frillish para o pescador",
+    :QuestGiver => "Pescador do Liberty Pier.",
+    :Stage1 => "Encontre o tio do pescador.",
+    :Stage2 => "Leve um Frillish para o pescador.",
     :Location1 => "Nimbasa",
     :Location2 => "Castelia",
     :QuestDescription => "Um jovem pescador me pediu ajuda para capturar um Frillish para impressionar seu pai, devo arranjar um jeito de capturar um e entregar para ele.",
@@ -303,8 +303,8 @@ module QuestModule
     :ID => "25",
     :Name => "Grupo de dança",
     :QuestGiver => "Breakdancer",
-    :Stage1 => "Encontre dois outros breakdancers",
-    :Stage2 => "Reencontre o breakdancer",
+    :Stage1 => "Encontre dois outros breakdancers.",
+    :Stage2 => "Reencontre o breakdancer.",
     :Location1 => "Castelia",
     :Location2 => "Central Plaza",
     :QuestDescription => "Encontrei um breakdancer na Central Plaza, em Castelia, que quer montar um grupo de dança, ele me pediu para procurar outros breakdancers por Castelia que pudessem fazer parte do grupo.",
@@ -313,15 +313,15 @@ module QuestModule
   Quest26 = {
     :ID => "26",
     :Name => "Especialistas em Triple Triad",
-    :Stage1 => "Derrote os dois especialistas",
+    :Stage1 => "Derrote os dois especialistas.",
     :Location1 => "Game Corner",
-    :QuestDescription => "Descobri que existem dois jogadores profissionais de Triple Triad que desafiam outros jogadores a derrotá-los em jogos com regras especiais no Game Corner. Enfrentar eles pode ser uma boa ideia.",
+    :QuestDescription => "No Game Corner, descobri que existem dois jogadores profissionais de Triple Triad que desafiam outros jogadores em jogos com regras especiais. Enfrentar eles pode ser uma boa ideia.",
   }
 
   Quest27 = {
     :ID => "27",
     :Name => "Os Guardiões do Desert Resort",
-    :Stage1 => "Derrote ou capture os guardiões",
+    :Stage1 => "Derrote ou capture os guardiões.",
     :Location1 => "Desert Resort",
     :QuestDescription => "Existem cinco pokémon especiais espalhados pelo Desert Resort. Posso testar minhas habilidades desafiando eles, ou tentar capturá-los.",
   }
@@ -333,7 +333,7 @@ module QuestModule
   Quest29 = {
     :ID => "29",
     :Name => "Preso na Caverna dos Zubats",
-    :Stage1 => "Encontre uma saída",
+    :Stage1 => "Encontre uma saída.",
     :Location1 => "Caverna dos Zubats",
     :QuestDescription => "Encontrei uma caverna no Desert Resort e acabei preso dentro dela, pra piorar, o lugar é cheio de zubats.",
   }
@@ -342,9 +342,9 @@ module QuestModule
     :ID => "30",
     :Name => "Infestação de Vibravas",
     :QuestGiver => "Hiker",
-    :Stage1 => "Derrote 7 vibravas",
-    :Stage2 => "Converse com o hiker",
-    :Stage3 => "Receba a recompensa com o Líder",
+    :Stage1 => "Derrote 7 vibravas.",
+    :Stage2 => "Converse com o hiker.",
+    :Stage3 => "Receba a recompensa com o Líder.",
     :Location1 => "Desert Resort",
     :Location2 => "Clube de Hikers",
     :Location3 => "Clube de Hikers",
@@ -355,9 +355,9 @@ module QuestModule
     :ID => "31",
     :Name => "Wailord na praia",
     :QuestGiver => "Marujo hiker",
-    :Stage1 => "Encontre e derrote o wailord",
-    :Stage2 => "Converse com o marinheiro",
-    :Stage3 => "Receba a recompensa com o Líder",
+    :Stage1 => "Encontre e derrote o wailord.",
+    :Stage2 => "Converse com o marinheiro.",
+    :Stage3 => "Receba a recompensa com o Líder.",
     :Location1 => "Desert Resort",
     :Location2 => "Clube de Hikers",
     :Location3 => "Clube de Hikers",
@@ -368,7 +368,7 @@ module QuestModule
     :ID => "32",
     :Name => "Registrando a Rota 4",
     :QuestGiver => "Hiker idoso",
-    :Stage1 => "Capture todas as espécies da Rota 4",
+    :Stage1 => "Capture todas as espécies da Rota 4.",
     :Location1 => "Rota 4",
     :QuestDescription => "Um senhor no Clube de Hikers me pediu para registrar todos as espécies que habitam na Rota 4. Devo capturar todos os pokemon e mostrar para o Chefe seus registros.",
   }
@@ -377,8 +377,8 @@ module QuestModule
     :ID => "33",
     :Name => "Inspeção em A Taverna",
     :QuestGiver => "Delegacia de Castelia",
-    :Stage1 => "Investigue A Taverna",
-    :Stage2 => "Relate as informações adquiridas",
+    :Stage1 => "Investigue A Taverna.",
+    :Stage2 => "Relate as informações adquiridas.",
     :Location1 => "Gym Street",
     :Location2 => "Delegacia de Castelia",
     :QuestDescription => "Na Delegacia de Castelia, aceitei a missão de ajudar os policiais a investigar A Taverna, um tipo de bar feito e frequentado apenas por Motoqueiros. Um cientista disfarçado me espera lá para ajudar a conseguir mais informações.",
@@ -388,8 +388,8 @@ module QuestModule
     :ID => "34",
     :Name => "Dragões na Passagem",
     :QuestGiver => "Hiker arqueólogo",
-    :Stage1 => "Atravesse a Relic Passage",
-    :Stage2 => "Derrote o Hydreigon",
+    :Stage1 => "Atravesse a Relic Passage.",
+    :Stage2 => "Derrote o Hydreigon.",
     :Location1 => "Relic Passage",
     :Location2 => "Relic Passage",
     :QuestDescription => "Um hiker arqueólogo me pediu para atravessar a Relic Passage junto com ele enquanto enfrentamos Deinos. Pode ser que a Relic Passage também tenha um Hydreigon, que pode causar grandes dificuldades para outros treinadores que queiram atravessarem a caverna.",
